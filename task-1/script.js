c05f7334780e701aae1fbf09d900e174c87abf8a
Al-Motaz Bellah Ahmed <138595163+MotazAzami@users.noreply.github.com>
2025-07-03 14:29:52 +0300

    adding factory function & constructor fucntion

--- task-1/script.js
+++ task-1/script.js
@@ -1,2 +1,45 @@
-console.log("hello world");
-console.log("hello world");
+// 1. FACTORY FUNCTION
+function createBook(title, author) {
+  return {
+    title: title,
+    author: author,
+    isRead: false,
+
+    toggleReadStatus() {
+      this.isRead = !this.isRead;
+    },
+
+    describe() {
+      return `"${this.title}" By ${this.author} [${
+        this.isRead ? "Read" : "Unread"
+      }]`;
+    },
+  };
+}
+
+let factoryBook = createBook("home alone", "motaz");
+console.log(factoryBook.describe());
+factoryBook.toggleReadStatus();
+console.log(factoryBook.describe());
+
+// 2. CONSTRUCTOR FUNCTION
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+  this.isRead = false;
+}
+
+Book.prototype.toggleReadStatus = function () {
+  this.isRead = !this.isRead;
+};
+
+Book.prototype.describe = function () {
+  return `"${this.title}" By ${this.author} [${
+    this.isRead ? "Read" : "Unread"
+  }]`;
+};
+
+let constructorBook = new Book("home alone", "motaz");
+console.log(constructorBook.describe());
+constructorBook.toggleReadStatus();
+console.log(constructorBook.describe());
