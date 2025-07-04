eff1d58cc6287cfd370c45b2eab09769b931e64e
Al-Motaz Bellah Ahmed <138595163+MotazAzami@users.noreply.github.com>
2025-07-04 22:12:33 +0300

    Refactors book creation methods
    Improves code clarity and demonstrates different ways
    to create objects: arrow function, factory function,
    constructor function, and class.
    
    Adds a description and read status toggle to each book object.

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-// 1. FACTORY FUNCTION
-function createBook(title, author) {
+// 1. ARROW FUNCTION
+const createBook = (title, author) => ({
+  title,
+  author,
+  isRead: false,
+  toggleReadStatus() {
+    this.isRead = !this.isRead;
+  },
+  describe() {
+    return `"${this.title}" by ${this.author} [${this.isRead ? "Read" : "Unread"}]`;
+  }
+});
+
+let arrowBookObject = createBook("home alone", "motaz");
+console.log(arrowBookObject.describe());
+arrowBookObject.toggleReadStatus();
+console.log(arrowBookObject.describe());
+
+
+// 2. FACTORY FUNCTION
+function createBookObject(title, author) {
   return {
     title: title,
     author: author,
@@ -17,34 +36,34 @@ function createBook(title, author) {
   };
 }
 
-let factoryBook = createBook("home alone", "motaz");
-console.log(factoryBook.describe());
-factoryBook.toggleReadStatus();
-console.log(factoryBook.describe());
+let factoryBookObject = createBookObject("home alone", "motaz");
+console.log(factoryBookObject.describe());
+factoryBookObject.toggleReadStatus();
+console.log(factoryBookObject.describe());
 
-// 2. CONSTRUCTOR FUNCTION
-function Book(title, author) {
+// 3. CONSTRUCTOR FUNCTION
+function constructorBook(title, author) {
   this.title = title;
   this.author = author;
   this.isRead = false;
 }
 
-Book.prototype.toggleReadStatus = function () {
+constructorBook.prototype.toggleReadStatus = function () {
   this.isRead = !this.isRead;
 };
 
-Book.prototype.describe = function () {
+constructorBook.prototype.describe = function () {
   return `"${this.title}" By ${this.author} [${
     this.isRead ? "Read" : "Unread"
   }]`;
 };
 
-let constructorBook = new Book("home alone", "motaz");
-console.log(constructorBook.describe());
-constructorBook.toggleReadStatus();
-console.log(constructorBook.describe());
+let constructorBookObject = new constructorBook("home alone", "motaz");
+console.log(constructorBookObject.describe());
+constructorBookObject.toggleReadStatus();
+console.log(constructorBookObject.describe());
 
-// 3. CLASS
+// 4. CLASS
 class classBook {
   constructor (title, author) {
     this.title = title;
